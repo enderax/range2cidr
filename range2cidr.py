83aa90ddbc5ab823027fcba46035a89a1ee267f4
enderax <ender@silent-root.local>
2015-07-02 10:44:37 +0300

    Fix
    Comment issue fixed

--- range2cidr.py
+++ range2cidr.py
@@ -66,8 +66,7 @@ parser.add_argument('-f','--file', dest='filename', required=True,
                    help='The file that contains IP ranges')
 parser.add_argument('-o','--outfile', dest='outfile',
                    help='Output file')
-#parser.add_argument('-u','--url', dest='url',
-                   help='URL of list')
+#parser.add_argument('-u','--url', dest='url', help='URL of list')
 
 args = parser.parse_args()
 
@@ -81,6 +80,7 @@ else:
 
     if args.outfile:
         fo = open(args.outfile,'w+')
+        print "Converting..."
         for ipx in ips:
             fo.write(calculate(ipx)+'\n')
         fo.close()
